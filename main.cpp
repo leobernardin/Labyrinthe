cf3fb4533e2338f4c84341306249861df022bcaa
ridley809 <44375311+ridley809@users.noreply.github.com>
2024-12-02 19:44:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,9 +13,27 @@
 int main(){
 
 
+   
+    
+    //pour tester les constructeurs : 
+    /*
     Labyrinthe l(11,11);
+    cout<<"Labyrinthe 1 : \n";
     l.afficher();
-
+    //constructeur defaut
+	Labyrinthe l2;
+    cout<<"Labyrinthe 2 : \n";
+    l2.afficher();
+    
+    Labyrinthe l3;
+    cout<<"Labyrinthe 3 apres copie du labyrinthe 1 : \n";
+    l3 = l;
+    l3.afficher();
+    */
+	
+	//main principal
+	Labyrinthe l(11,11);
+	l.afficher();
     char reponse;
 
     while (l.fini() == 0){
@@ -102,6 +120,5 @@ int main(){
 		cout << "Vous avez Perdu. L'ennemi vous a trouve " << endl;
 
 	}
-
 
 }
